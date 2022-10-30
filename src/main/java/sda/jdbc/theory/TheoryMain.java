6bdf710c3a04d55bd7602d0ac8e687dd046ed77d
Sorin Costiuc <sorin.costiuc90@gmail.com>
2022-10-30 14:26:07 +0200

    Insert, insert with parameters, select all and update

--- src/main/java/sda/jdbc/theory/TheoryMain.java
+++ src/main/java/sda/jdbc/theory/TheoryMain.java
@@ -1,9 +1,6 @@
 package sda.jdbc.theory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TheoryMain {
     private static final String urlDB = "jdbc:mysql://localhost:3306/sda_database";
@@ -64,7 +61,7 @@ public class TheoryMain {
     public static void executeStatement() {
         try (Connection con = DriverManager.getConnection(urlDB, userNameDB, userPasswordDB)) {
             try (Statement statement = con.createStatement()) {
-                String tableSql = "CREATE TABLE IF NOT EXISTS emplyees" + "(" +
+                String tableSql = "CREATE TABLE IF NOT EXISTS employees" + "(" +
                         "emp_id int PRIMARY KEY AUTO_INCREMENT, " +
                         "name varchar(30), " +
                         "position varchar(30), " +
@@ -80,8 +77,90 @@ public class TheoryMain {
         }
     }
 
+    //    no parameter to insert and put value in maethod
+    public static void insertEmployee() {
+        try (Connection con = DriverManager.getConnection(urlDB, userNameDB, userPasswordDB)) {
+            try (Statement statement = con.createStatement()) {
+                String sql = "INSERT INTO employees(name, position, salary)" +
+                        "VALUES('Sorin', 'Manager', 3000), ('Bogdan', 'CEO', 2000)";
+                System.out.println("Put values in table: " + sql);
+
+                int affectedRows = statement.executeUpdate(sql);
+
+                System.out.println("Command successfully executed. Affected rows: " + affectedRows);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    parameter to insert
+    public static void insertEmpWithParam(String name, String position, double salary) {
+        try (Connection con = DriverManager.getConnection(urlDB, userNameDB, userPasswordDB)) {
+            String sql = "INSERT INTO employees(name, position, salary) VALUES(?, ?, ?)";
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setString(1, name);
+                ps.setString(2, position);
+                ps.setDouble(3, salary);
+
+                int affectedRows = ps.executeUpdate();
+
+                System.out.println("Command successfully executed. Affected rows: " + affectedRows);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectAllFromTable() {
+        try (Connection con = DriverManager.getConnection(urlDB, userNameDB, userPasswordDB)) {
+            try (Statement s = con.createStatement()) {
+                String sql = "SELECT * FROM employees";
+                try (ResultSet resultSet = s.executeQuery(sql)) {
+                    while (resultSet.next()) {
+                        int empId = resultSet.getInt("emp_id");
+                        String name = resultSet.getString("name");
+                        String position = resultSet.getString("position");
+                        double salary = resultSet.getDouble("salary");
+
+                        System.out.println("Emp -> emp_id: " + empId + ", name:" + name + ", position:" + position + ", salary: " + salary);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateSalaryForEmp(int empId, double salary) {
+        try (Connection con = DriverManager.getConnection(urlDB, userNameDB, userPasswordDB)) {
+            String sql = "UPDATE employees SET salary = ? WHERE emp_id = ?";
+            try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setDouble(1, salary);
+                ps.setInt(2, empId);
+
+                System.out.println("Execute update: " + sql);
+                int affectedRows = ps.executeUpdate();
+
+                System.out.println("Update successfully executed. Affected rows: " + affectedRows);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     public static void main(String[] args) {
 //        connectToDB();
-        executeStatement();
+//        executeStatement();
+//        insertEmployee();
+//        insertEmpWithParam("Dany", "trainer", 1000);
+//        insertEmpWithParam("Marius", "army boy", 300);
+        System.out.println("Before");
+        selectAllFromTable();
+        updateSalaryForEmp(1, 8000);
+        System.out.println("After");
+        selectAllFromTable();
     }
 }
