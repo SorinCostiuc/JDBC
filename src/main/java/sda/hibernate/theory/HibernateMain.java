c507d8f820b78675c7a61e85c2c6ae98fe20c969
Sorin Costiuc <sorin.costiuc90@gmail.com>
2022-11-05 13:15:37 +0200

    Hibernate create config and use session - refactoring and extracting method to insert new value

--- src/main/java/sda/hibernate/theory/HibernateMain.java
+++ src/main/java/sda/hibernate/theory/HibernateMain.java
@@ -8,7 +8,7 @@ import sda.hibernate.theory.entity.Employee;
 
 
 public class HibernateMain {
-    public static void insertEmployee() {
+    public static void insertEmployee(Employee... employees) {
         System.out.println("Persist employee ...");
         try (SessionFactory sf = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -16,13 +16,9 @@ public class HibernateMain {
 
             try (Session session = sf.openSession()) {
                 Transaction transaction = session.beginTransaction();
-                Employee employee1 = new Employee();
-                employee1.setName("Sorin");
-                employee1.setPosition("Manager");
-                employee1.setSalary(4000);
-
-                session.persist(employee1);
-
+                for (Employee employee : employees) {
+                    session.persist(employee);
+                }
                 transaction.commit();
             }
         }
@@ -30,6 +26,17 @@ public class HibernateMain {
 
 
     public static void main(String[] args) {
-        insertEmployee();
+        Employee employee1 = new Employee();
+        employee1.setName("Sorin");
+        employee1.setPosition("Manager");
+        employee1.setSalary(4000);
+
+        Employee employee2 = new Employee();
+        employee2.setName("Bogdan");
+        employee2.setPosition("Scrum");
+        employee2.setSalary(3000);
+
+        insertEmployee(employee1, employee2);
+        System.out.println("Heck yeah");
     }
 }
